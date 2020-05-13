d22bdcad3246184a110c905e881009b9e2db6e18
Samuel Henrique <samu.henr@gmail.com>
2020-05-13 10:23:05 -0300

    tests appointment

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentServices';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12345',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  it('should not be able to create two appointment on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date();
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '12345',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '12345',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
