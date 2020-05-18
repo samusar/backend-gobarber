cec6dfebcab131aff77076e0a692a6f5c8196277
Samuel Henrique <samu.henr@gmail.com>
2020-05-18 17:44:05 -0300

    Add column user_id in appointment table

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -19,6 +19,7 @@ describe('CreateAppointment', () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '12345',
+      user_id: '54321',
     });
 
     expect(appointment).toHaveProperty('id');
@@ -30,12 +31,14 @@ describe('CreateAppointment', () => {
     await createAppointment.execute({
       date: appointmentDate,
       provider_id: '12345',
+      user_id: '54321',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '12345',
+        user_id: '54321',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
